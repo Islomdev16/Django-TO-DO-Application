3f433a5c9ce180312f74198fc3b8a0cc615d5c74
Islomdev16 <jekinsma1604@gmail.com>
2022-04-29 04:22:44 +0500

    model is ready and tasks added

--- Django TODO 1/todo_list/admin.py
+++ Django TODO 1/todo_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+@admin.register(Task)
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ["user", "title", "complete", "created"]
+    list_filter = ["complete"]
