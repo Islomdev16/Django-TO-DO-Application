90e7e3c5593fb5d7ee56acdf5785edd123317f87
Islomdev16 <jekinsma1604@gmail.com>
2022-04-29 15:15:32 +0500

    changed added

--- Django TODO 1/todo_list/views.py
+++ Django TODO 1/todo_list/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from .models import Task
 
 # Create your views here.
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
 
-def taskList(request):
-    return HttpResponse("To Do List")
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "todo_list/task.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = "todo_list/task_form.html"
