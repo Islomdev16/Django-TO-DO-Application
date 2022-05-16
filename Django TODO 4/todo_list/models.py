905b83f022e25c063f2e6edd9821fe91c5e438cc
Islomdev16 <jekinsma1604@gmail.com>
2022-05-17 04:24:01 +0500

    home page frontend fully made

--- Django TODO 4/todo_list/models.py
+++ Django TODO 4/todo_list/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     time = models.DateTimeField()
 
     def __str__(self):
