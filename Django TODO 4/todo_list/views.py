666aa8fb49e643a0a86ae8bd88eac22753e05d20
Islomdev16 <jekinsma1604@gmail.com>
2022-05-17 22:27:47 +0500

    updateed

--- Django TODO 4/todo_list/views.py
+++ Django TODO 4/todo_list/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from .models import Todo
 
 # Create your views here.
 def home(request):
+    todo_items = Todo.objects.all().order_by('-time')
+    context = {
+        "todo_items":todo_items
+    }
+    return render(request, 'index.html', context)
+
+@csrf_exempt
+def add_todo(request):
+    current_date = timezone.now()
+    title = request.POST['title']
+    desc = request.POST['desc']
+    created_obj = Todo.objects.create(time=current_date, text=title, description=desc)
+    length_of_todos = Todo.objects.all().count()
+    print(length_of_todos)
+
     return render(request, 'index.html')
+
 
