a46462251f5cb3abd8623943f4370eb33cc700ce
Islomdev16 <jekinsma1604@gmail.com>
2022-05-18 16:23:12 +0500

    changes made

--- Django TODO 4/todo_list/views.py
+++ Django TODO 4/todo_list/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.utils import timezone
 from .models import Todo
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
-    todo_items = Todo.objects.all().order_by('-time')
+    todo_items = Todo.objects.all().order_by('-added_time')
     context = {
         "todo_items":todo_items
     }
@@ -15,11 +16,7 @@ def home(request):
 def add_todo(request):
     current_date = timezone.now()
     title = request.POST['title']
-    desc = request.POST['desc']
-    created_obj = Todo.objects.create(time=current_date, text=title, description=desc)
+    new_task = Todo.objects.create(added_time=current_date, title=title)
     length_of_todos = Todo.objects.all().count()
-    print(length_of_todos)
-
-    return render(request, 'index.html')
-
+    return HttpResponseRedirect('/')
 
