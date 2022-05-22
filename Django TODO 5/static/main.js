7fa93ec161a2deedb56cb3ecea968e003a01a2ac
Islomdev16 <jekinsma1604@gmail.com>
2022-05-22 14:58:20 +0500

    js updated

--- Django TODO 5/static/main.js
+++ Django TODO 5/static/main.js
@@ -9,13 +9,13 @@ function getTodos(){
         for(let d of data){
             let listContainer = `
             <div class="wrapper">
-                <div class="wrapperText"><p>${d.title} </p></div>
-                <div class="iconContainer">
+                <div style="margin-top: 1px;"><p>Lorem ipsum dolor sit amet</p></div>
+                <div class="d-flex">
                     <div class="editbtn"><i class="fas fa-pen-alt"></i></div>
                     <div class="delbtn"><i class="fas fa-trash"></i></div>
                 </div>
             </div>
-            `
+        `
             wrapper.innerHTML += listContainer
         }
     })
@@ -26,4 +26,22 @@ form.addEventListener('submit', function(e){
     e.preventDefault()
     let inputValue = document.getElementById('input').value
     console.log(inputValue)
+
+    const data = {title: "inputValue"};
+    let url = 'http://127.0.0.1:8000/api/create_todo'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+
+    .then(data => {
+        console.log("Success:", data);
+    })
+//    .catch((error) => {
+//    console.error("Error:", error);
+//    });
 })
