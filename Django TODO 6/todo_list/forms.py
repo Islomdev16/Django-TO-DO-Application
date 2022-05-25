b22e67b50f35aff37335630b5ffe56d331c63d63
Islomdev16 <jekinsma1604@gmail.com>
2022-05-25 15:54:25 +0500

    query added

---
+++ Django TODO 6/todo_list/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Mytodo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Mytodo
+        fields = "__all__"
+
--- Django TODO 6/todo_list/views.py
+++ Django TODO 6/todo_list/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from .models import Mytodo
+from .forms import TodoForm
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    tasks = Mytodo.objects.all()
+    form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+    return render(request, 'index.html', context)
+
