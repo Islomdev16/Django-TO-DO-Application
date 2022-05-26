e72991937ba9f4a135fd79d5e5b48b0f2852cd44
Islomdev16 <jekinsma1604@gmail.com>
2022-05-26 07:18:27 +0500

    css added

--- Django TODO 6/todo_list/forms.py
+++ Django TODO 6/todo_list/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Mytodo
 
 class TodoForm(forms.ModelForm):
+    task = forms.CharField(max_length=50, widget=forms.TextInput(attrs={}))
     class Meta:
         model = Mytodo
         fields = "__all__"
