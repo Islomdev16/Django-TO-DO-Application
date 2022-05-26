45bd477901956f7e495e688c29a8b83e74c06740
Islomdev16 <jekinsma1604@gmail.com>
2022-05-26 17:36:07 +0500

    finished project

--- Django TODO 6/todo_list/forms.py
+++ Django TODO 6/todo_list/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from .models import Mytodo
 
 class TodoForm(forms.ModelForm):
-    task = forms.CharField(max_length=50, widget=forms.TextInput(attrs={}))
+    task = forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+        'id': 'todoField', 'placeholder': 'Enter Task'
+    }))
+
     class Meta:
         model = Mytodo
         fields = "__all__"
