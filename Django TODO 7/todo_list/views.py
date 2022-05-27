de1a02e7061a6c353499f4ace69d4bd28506b641
Islomdev16 <jekinsma1604@gmail.com>
2022-05-27 22:44:50 +0500

    delete and add function added

--- Django TODO 7/todo_list/views.py
+++ Django TODO 7/todo_list/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    tasks = Todo.objects.all()
+    form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = TodoForm()
+
+    context = {
+        'tasks':tasks,
+        'form':form
+    }
+    return render(request, 'home.html', context)
+
+def delete_task(request, pk):
+    task = Todo.objects.get(id=pk)
+    task.delete()
+    return redirect('home')
+
