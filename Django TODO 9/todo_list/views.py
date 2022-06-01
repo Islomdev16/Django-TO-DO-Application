fd0d9fafc2d21018298d700580d0c2b74e116a01
Islomdev16 <jekinsma1604@gmail.com>
2022-06-01 23:32:30 +0500

    tasks finihed

--- Django TODO 9/todo_list/views.py
+++ Django TODO 9/todo_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,8 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    form = UserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'register.html', context)
