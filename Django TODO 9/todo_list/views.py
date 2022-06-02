34f1aa9e2bf14b843b917424c832b2449aecdf63
Islomdev16 <jekinsma1604@gmail.com>
2022-06-02 15:36:44 +0500

    register finished

--- Django TODO 9/todo_list/views.py
+++ Django TODO 9/todo_list/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,38 +9,39 @@ def home(request):
 def login(request):
     return render(request, 'login.html')
 
-def register(request):
-    if request.method == 'GET':
-        form = UserCreationForm()
-        context = {
-            'form':form
-        }
-        return render(request, 'register.html', context)
-    else:
-        form = UserCreationForm(request.POST)
-        context = {
-            "form":form
-        }
-        if form.is_valid():
-            user = form.save()
-            if user is not None:
-                return redirect('home')
-        else:
-            return render(request, 'register.html', context)
-
 # def register(request):
-#     form = UserCreationForm()
-#     if request.method == 'POST':
+#     if request.method == 'GET':
+#         form = UserCreationForm()
+#         context = {
+#             'form':form
+#         }
+#         return render(request, 'register.html', context)
+#     else:
 #         form = UserCreationForm(request.POST)
+#         context = {
+#             "form":form
+#         }
 #         if form.is_valid():
-#             form.save()
-#             return redirect('home')
+#             user = form.save()
+#             if user is not None:
+#                 return redirect('home')
 #         else:
-#             form = UserCreationForm()
-#
-#     context = {
-#         'form':form
-#     }
-#     return render(request, 'register.html', context)
+#             return render(request, 'register.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully for {username} !')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form':form
+    }
+    return render(request, 'register.html', context)
 
 
