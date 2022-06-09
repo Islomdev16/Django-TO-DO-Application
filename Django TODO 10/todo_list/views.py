439043992f700be44681c9e7a179a869694430c7
Islomdev16 <jekinsma1604@gmail.com>
2022-06-09 22:31:50 +0500

    Model created

--- Django TODO 10/todo_list/views.py
+++ Django TODO 10/todo_list/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'index.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'detail.html'
+
