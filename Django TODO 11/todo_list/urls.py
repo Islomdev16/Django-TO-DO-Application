8e02fb5a897b8cb5e5500c6f93d9ddfb5f6c4a5f
Islomdev16 <jekinsma1604@gmail.com>
2022-06-12 15:49:26 +0500

    Delete added

--- Django TODO 11/todo_list/urls.py
+++ Django TODO 11/todo_list/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.homeTask, name='home'),
     path('update/<int:pk>/', views.updateTask, name='update'),
+    path('delete/<int:pk>/', views.deleteTask, name='delete'),
 ]
