43d6936333a6e403d154e11f1b35b0eb3b05dd5d
Islomdev16 <jekinsma1604@gmail.com>
2022-06-15 21:25:24 +0500

    Models created

--- Django TODO 12/todo_list/views.py
+++ Django TODO 12/todo_list/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import MyTodo
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    tasks = MyTodo.objects.all().order_by('-created_at')
+    context = {
+        'tasks':tasks
+    }
+    return render(request, 'home.html', context)
