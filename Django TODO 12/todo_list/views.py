0337f35e3790f8d20f8d7cb51b6d467bdad27d16
Islomdev16 <jekinsma1604@gmail.com>
2022-06-17 21:48:14 +0500

    functional added

--- Django TODO 12/todo_list/views.py
+++ Django TODO 12/todo_list/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MyTodo
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        MyTodo.objects.create(title=title)
+        if title != '':
+            MyTodo.objects.create(title=title)
+        return redirect('home')
     tasks = MyTodo.objects.all().order_by("complete", '-created_at')
     context = {
         'tasks':tasks
